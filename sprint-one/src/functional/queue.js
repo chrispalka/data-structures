2648091ddf17c898b33d4f0cc4cfc0ac1f491608
Chris Palka <cpalka87gmail.com>
2020-04-06 01:31:05 -0600

    Intial refactor of functional/queue.js for part-one

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,32 @@
 var Queue = function() {
   var someInstance = {};
-
+  var count = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[count] = value;
+    count++;
+    return storage;
   };
 
   someInstance.dequeue = function() {
+    var lastItem = 0;
+    for (var k in storage) {
+      lastItem++;
+    }
+    count--;
+    return delete storage[lastItem - 1];
   };
 
   someInstance.size = function() {
+    var len = 0;
+    for (var k in storage) {
+      len++;
+    }
+    return len;
   };
 
   return someInstance;
