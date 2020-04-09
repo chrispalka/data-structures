2fdb6aba4da6e9b47af157ae1c25b03a1c32addd
Chris Palka <cpalka87gmail.com>
2020-04-08 21:58:32 -0600

    Complete refactor of stack.js..
    for functional-shared instantiation

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,37 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.count = 0;
+
+
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+  return this.storage;
+};
+stackMethods.pop = function() {
+  var deletedKey, deletedItem;
+  for (var k in this.storage) {
+    deletedKey = k;
+  }
+  deletedItem = this.storage[deletedKey];
+  delete this.storage[deletedKey];
+  this.count--;
+  return deletedItem;
+};
+stackMethods.size = function() {
+  var len = 0;
+  for (var k in this.storage) {
+    len++;
+  }
+  return len;
+};
 
 
