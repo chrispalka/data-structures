d059e66e10b0c7154ad86c2a76794e6bcc6a9822
Chris Palka <cpalka87gmail.com>
2020-04-11 21:50:20 -0600

    Complete prototypal stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,39 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.count = 0;
+  return someInstance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+  return this.storage;
+};
+
+queueMethods.dequeue = function() {
+  var deletedItem, deletedKey;
+  for (var k in this.storage) {
+    deletedKey = k;
+    break;
+  }
+  deletedItem = this.storage[deletedKey];
+  delete this.storage[deletedKey];
+  return deletedItem;
+};
+
+queueMethods.size = function() {
+  var len = 0;
+  for (var k in this.storage) {
+    len++;
+  }
+  return len;
+};
+
+var queue = Queue();
+
+
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,37 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  someInstance.count = 0;
+  return someInstance;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+  return this.storage;
+};
+
+stackMethods.pop = function() {
+  var deletedItem, deletedKey;
+  for (var k in this.storage) {
+    deletedKey = k;
+  }
+  deletedItem = this.storage[deletedKey];
+  delete this.storage[deletedKey];
+  this.count--;
+  return deletedItem;
+};
+
+stackMethods.size = function() {
+  var len = 0;
+  for (var k in this.storage) {
+    len++;
+  }
+  return len;
+};
+
+var stack = Stack();
 
 
