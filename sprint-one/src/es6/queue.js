bb20fc9153400153096caba30b8605b3704a6903
Chris Palka <cpalka87gmail.com>
2020-04-11 22:22:23 -0600

    Complete sprint one

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,34 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
   }
 
+  enqueue(value) {
+    this.storage[this.count] = value;
+    this.count++;
+    return this.storage[this.count];
+  }
+
+  dequeue() {
+    var deletedItem, deletedKey;
+    for (var k in this.storage) {
+      deletedKey = k;
+      break;
+    }
+    deletedItem = this.storage[deletedKey];
+    delete this.storage[deletedKey];
+    return deletedItem;
+  }
+
+  size() {
+    var len = 0;
+    for (var k in this.storage) {
+      len++;
+    }
+    return len;
+  }
 }
+
+var queue = new Queue();
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,34 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
   }
 
+  push(value) {
+    this.storage[this.count] = value;
+    this.count++;
+    return this.storage[this.count];
+  }
+
+  pop() {
+    var deletedItem, deletedKey;
+    for (var k in this.storage) {
+      deletedKey = k;
+    }
+    deletedItem = this.storage[deletedKey];
+    delete this.storage[deletedKey];
+    this.count--;
+    return deletedItem;
+  }
+
+  size() {
+    var len = 0;
+    for (var k in this.storage) {
+      len++;
+    }
+    return len;
+  }
 }
+
+var stack = new Stack();
