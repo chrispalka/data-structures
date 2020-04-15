fd4c45b42c01cd8c9bcdf92c2d9bc02a9930c416
Chris Palka <cpalka87gmail.com>
2020-04-14 21:28:04 -0600

    Complete Binary Search Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,60 @@
 var BinarySearchTree = function(value) {
+  var newTree = {};
+  newTree.value = value;
+  newTree.left = null;
+  newTree.right = null;
+
+  _.extend(newTree, treeMeth);
+  return newTree;
 };
 
+var treeMeth = {};
 
+treeMeth.insert = function(value) {
+  var newLevel = BinarySearchTree(value);
+  if (this.value > value) {
+    if (this.left === null) {
+      this.left = newLevel;
+    } else {
+      this.left.insert(value);
+    }
+  }
+  if (this.value < value) {
+    if (this.right === null) {
+      this.right = newLevel;
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+treeMeth.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+  if (this.value > target) {
+    if (this.left !== null) {
+      return this.left.contains(target);
+    }
+  } else if (this.value < target) {
+    if (this.right !== null) {
+      return this.right.contains(target);
+    }
+  }
+  return false;
+};
+
+treeMeth.depthFirstLog = function(func) {
+  func(this.value);
+  if (this.left !== null) {
+    this.left.depthFirstLog(func);
+  }
+  if (this.right !== null) {
+    this.right.depthFirstLog(func);
+  }
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// all 3 functions are = O(n)
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -37,5 +37,5 @@ treeMethods.contains = function(target) {
  * Complexity: What is the time complexity of the above functions?
  */
 
- // treeMethods.addChild = O(1)
- // treeMethods.contains = O(n^2)
+// treeMethods.addChild = O(1)
+// treeMethods.contains = O(n^2)
