f619e15b64ca1453c3e55fb39c073c42a939a41a
Chris Palka <cpalka87gmail.com>
2020-04-14 20:33:50 -0600

    Complete Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,44 @@
 var LinkedList = function() {
   var list = {};
-  list.head = null;
-  list.tail = null;
+  list.head = null; // should be first value
+  list.tail = null; // should be node where next is null
+
+  // n1 = {data: 100}
+
+  // n2 = {data: 200}
+
+  // n1.next = n2
 
   list.addToTail = function(value) {
+    var newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail = newNode;
+      this.head.next = newNode;
+    }
+    // {'item' : 'link' -> }
+    // {value: 3, next: {value: 7, next: {value: 2, next: null}}}
   };
 
   list.removeHead = function() {
+    var deletedNode = this.head;
+    this.head = this.head.next;
+    return deletedNode.value;
   };
 
   list.contains = function(target) {
+    if (this.head.value === target || this.tail.value === target) {
+      return true;
+    } else {
+      for (var key in this.next) {
+        if (this.next.value === target) {
+          return true;
+        }
+      }
+      return false;
+    }
   };
 
   return list;
