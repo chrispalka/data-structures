f14357fada932a6a557eb1ad7a763c09136a171a
Chris Palka <cpalka87gmail.com>
2020-04-15 21:40:58 -0600

    Initial refactor of graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,18 +2,26 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.storage = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.storage[node] = {};
+  this.storage[node]['edge'] = [];
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  if (this.storage[node]) {
+    return true;
+  }
+  return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  delete this.storage[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
@@ -30,6 +38,8 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  var allNodes = Object.keys(this.storage);
+  allNodes.forEach(element => cb(parseInt(element)));
 };
 
 /*
