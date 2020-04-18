0bacb44d513d50c0ee5aa4b7fffd0430df071012
Chris Palka <cpalka87gmail.com>
2020-04-18 13:23:22 -0600

    Continued refactor of hashTable.js collision spec

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,14 +1,20 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._tupleList = {};
+  this._tupleList.tupleHead = null;
+  this._tupleList.tupleTail = null;
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  if (this.retrieve(index)) {
+    this._tupleList.tupleHead.next = this._storage.set(index, v);
+  } else {
+    this._storage.set(index, v);
+  }
 };
+
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
@@ -20,10 +26,63 @@ HashTable.prototype.remove = function(k) {
   this._storage.set(index, undefined);
 };
 
-
-
 /*
  * Complexity: What is the time complexity of the above functions?
  */
 
 
+/*
+var LinkedList = function() {
+  var list = {};
+  list.head = null; // should be first value
+  list.tail = null; // should be node where next is null
+
+  // n1 = {data: 100}
+
+  // n2 = {data: 200}
+
+  // n1.next = n2
+  list.addToTail = function(value) {
+    var newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail = newNode;
+      this.head.next = newNode;
+    }
+    // {'item' : 'link' -> }
+    // {value: 3, next: {value: 7, next: {value: 2, next: null}}}
+  };
+
+  list.removeHead = function() {
+    var deletedNode = this.head;
+    this.head = this.head.next;
+    return deletedNode.value;
+  };
+
+  list.contains = function(target) {
+    if (this.head.value === target || this.tail.value === target) {
+      return true;
+    } else {
+      for (var key in this.next) {
+        if (this.next.value === target) {
+          return true;
+        }
+      }
+      return false;
+    }
+  };
+
+  return list;
+};
+
+var Node = function(value) {
+  var node = {};
+
+  node.value = value;
+  node.next = null;
+
+  return node;
+};
+*/
